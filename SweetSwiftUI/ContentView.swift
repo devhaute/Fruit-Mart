e2b38630a2afcaad1c2f5a3863a078faf0e0a67d
devhaute <devhaute@gmail.com>
2021-11-07 20:45:01 +0900

    도형 만들기 구현

--- SweetSwiftUI/ContentView.swift
+++ SweetSwiftUI/ContentView.swift
@@ -2,8 +2,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("도형 만들기")
+                .font(.system(size: 30, weight: .bold))
+            
+            Spacer()
+                .frame(height: 10)
+            
+            VStack(alignment: .leading) {
+                Text("둥근 모양")
+                    .font(.system(size: 20))
+                
+                ZStack {
+                    Rectangle().frame(height: 10)
+                    
+                    HStack {
+                        Circle()
+                            .fill(.gray)
+                        
+                        Ellipse()
+                            .fill(.gray)
+                        
+                        Capsule()
+                            .fill(.gray)
+                        
+                        RoundedRectangle(cornerRadius: 30)
+                            .fill(.gray)
+                    }
+                }
+            }
+            
+            VStack(alignment: .leading) {
+                Text("각진 모양")
+                    .font(.system(size: 20))
+                
+                ZStack {
+                    Rectangle().frame(height: 10)
+                    
+                    HStack {
+                        Rectangle()
+                            .fill(.gray)
+                        Rectangle()
+                            .fill(.gray)
+                        Rectangle()
+                            .fill(.gray)
+                    }
+                }
+            }
+        }
+        .padding([.leading, .trailing, .bottom])
     }
 }
 
